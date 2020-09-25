928ab42ccaa745d5d6605a09890c8003287f74f8
Rodrigo Toledo <rodrigotoledo@192.168.0.102>
2020-09-25 09:27:58 -0300

    Header of layout, but without styles

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Main from './components/main';
 
 function App() {
   return (
-    <div className="App">
+    <Main>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,7 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </Main>
   );
 }
 
---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div>Hello</div>
+    </>
+  )
+}
+export default Header;
